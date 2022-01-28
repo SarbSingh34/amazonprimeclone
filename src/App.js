d3fef23d258849fa05b52e5b0e9cb3a7a36e4d2b
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-01-28 15:34:12 +0530

    Routing Applied

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import  { BrowserRouter as Router ,Routes ,Route  } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes> 
+      
+         <Route path = "/checkout" element = {<Checkout />}> </Route>
+         <Route path = "/login"    element = {<Login/>}>  </Route>
+         <Route path = ""          element = {<Homepage/>}> </Route>
+      </Routes>
+    </Router>
   );
 }
 
+
+function Checkout()
+{
+  return<h1> This is the Checkout  Page.... </h1>
+}
+function Login()
+{
+  return<h1> This is the Login Page.... </h1>
+}
+function Homepage()
+{
+  return<h1> This is the Main  Home Page.... </h1>
+}
 export default App;

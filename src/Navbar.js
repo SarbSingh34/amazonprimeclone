5ab53159107a06adcca951ca0cfd15df65e8fcb7
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-01-29 06:30:00 +0530

    Uploaded

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,50 +4,79 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => 
 {
-  return(
-    <div className='main_nav' style = {{display:'flex',backgroundColor:'black',height:'9vh'}}>
-        
-           <div className="first_logo" style = {{width:'11%'}}>
-              <img src = "/images/amazon img.jpg" style = {{width:'68%',height:'9vh'}} />
+  return (
+    <div
+      className="main_nav"
+      style={{ display: "flex", backgroundColor: "black", height: "9vh" }}
+    >
+      <div className="first_logo" style={{ width: "11%" }}>
+        <img
+          src="/images/amazon img.jpg"
+          style={{ width: "68%", height: "9vh" }}
+        />
+      </div>
+
+      <div className="search_bar" style={{ width: "52%" }}>
+        <div
+          className="second_content"
+          style={{ display: "flex", paddingTop: "2%" }}
+        >
+          <div className="search_icon" style={{ width: "74%" }}>
+            <input
+              type="text"
+              className="search_text"
+              style={{ padding: "1% 36%" }}
+            />
           </div>
+          <div className="search_icon2">
+            <SearchIcon
+              className="search_icon3"
+              style={{ backgroundColor: "aquamarine", padding: "4px" }}
+            />
+          </div>
+        </div>
+      </div>
 
-           <div className="search_bar" style = {{width:'52%'}}>
-                <div className="second_content" style = {{display:'flex',paddingTop:'2%'}}>
-                   <div className="search_icon" style = {{width:'74%'}}>
-                       <input type = "text" className = "search_text" style = {{padding:'1% 36%'}} />
-                   </div>
-                      <div className="search_icon2">
-                         <SearchIcon  className = "search_icon3" style = {{backgroundColor:'aquamarine',padding:'4px'}}/>
-                    </div>
-                </div>
-                  </div>
+      <div
+        className="side_content"
+        style={{
+          width: "20%",
+          display: "flex",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Link to="/">
+          <div
+            className="header_option"
+            style={{ display: "flex", flexDirection: "column", color: "white" }}
+          >
+            <span className = "firstline"> Hello </span>
+            <span className = "secondline"> Sign in </span>
+          </div>
+        </Link>
 
-            <div className="side_content" style = {{width:'20%',display:'flex',justifyContent:'space-evenly'}}>
-                   <Link to = "/">
-                       <div className="header_option" style = {{display:'flex',flexDirection:'column',color:'white'}}>
-                          <span> Hello  </span>
-                          <span> Sign in </span>
-                       </div>
-                        
-                   </Link>
+        <Link to="/">
+          <div
+            className="header_option"
+            style={{ display: "flex", flexDirection: "column", color: "white" }}
+          >
+            <span className = "firstline" style = {{fontSize:'12px'}}> Returns </span>
+            <span className = "secondline" style = {{fontSize:'19px',fontWeight:'500'}}> & Orders </span>
+          </div>
+        </Link>
 
-                   <Link to = "/">
-                   <div className="header_option" style = {{display:'flex',flexDirection:'column',color:'white'}}>
-                          <span> Returns   </span>
-                          <span> &  Orders  </span>
-                   </div>
-                        
-                   </Link>
-                   <Link to = "/">
-                   <div className="header_option" style = {{display:'flex',flexDirection:'column',color:'white'}}>
-                     <span>  Your   </span>
-                     <span>  Prime  </span>
-                    </div>
-                      
-                   </Link>
-            </div>
+        <Link to="/">
+          <div
+            className="header_option"
+            style={{ display: "flex", flexDirection: "column", color: "white" }}
+          >
+            <span className = "firstline"> Your </span>
+            <span className = "secondline"> Prime </span>
+          </div>
+        </Link>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Navbar;

ec827ce5651a5c8c582d07b4181e89d78321cd98
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-01-30 16:47:15 +0530

    Uploaded

--- src/Loginpg.js
+++ src/Loginpg.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React , { useState } from 'react';
+import { auth } from './Firebase';
 
 const Loginpg = () => 
 {
+ const [email,setemail] =  useState('');
+ const [pass,setpass] =  useState('');
+
+  function singin(e)
+  {
+     e.preventDefault();
+  }
+
+//   ---
+
+  function register(e)
+  {
+     e.preventDefault();
+
+     auth.createUserWithEmailAndPassword(email,pass)
+     .then((auth) =>
+     {
+       console.log(auth);
+     })
+     .catch(error =>alert(error.message))
+  }
   return (
       <>
          <div className="login_container" style = {{textAlign:'center',paddingTop:'2%'}} >
@@ -9,15 +31,16 @@ const Loginpg = () =>
                 <h1> Sign in  </h1>
                  <div className="main">
                       <span style = {{paddingRight:'10%'}}> Email: </span>
-                      <input type = "text" />
+                      <input type = "text"    value = {email}  onChange = { e => setemail(e.target.value) }   />
                  </div>
                  <div className="main">
                       <span> Password: </span>
-                      <input type = "password" />
+                      <input type = "password" value = {pass}  onChange = { e => setpass(e.target.value) }  />
                  </div>
-                 <button>SignIn</button>
+                 <button type = "submit" onClick={singin}>SignIn</button>
                  <p> By continuing, you agree to Amazon's Conditions of Use and Privacy Notice. </p>
-                 <button>Create your Amazon Account </button>
+
+                 <button type = "submit" onClick={register}> Create your Amazon Account </button>
              </div>
             
         </div>

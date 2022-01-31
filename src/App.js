3b95512e42ca1d5a2edc8a9a7db026bab02707c8
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-01-31 05:52:03 +0530

    Uploaded

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App()
      {
          auth.onAuthStateChanged(authUser =>
           {
-            console.log("The user is  ->>> , authUser");
+            console.log("The user is  ->>>" , authUser);
           })
         if(authUser)
         {

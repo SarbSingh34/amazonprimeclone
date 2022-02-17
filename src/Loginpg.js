71a69bd56b7bfc9ca831571ff9a7b606022fafb7
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-02-17 17:40:54 +0530

    Done Update

--- src/Loginpg.js
+++ src/Loginpg.js
@@ -42,22 +42,29 @@ const Loginpg = () =>
   return (
       <>
          <div className="login_container" style = {{textAlign:'center',paddingTop:'2%'}} >
-             <div className="main_content" style = {{backgroundColor:'brown',width:'17%',padding:'3% 2%'}}> 
-                <h1> Sign in  </h1>
-                 <div className="main">
-                      <span style = {{paddingRight:'10%'}}> Email: </span>
-                      <input type = "text"    value = {email}  onChange = { e => setemail(e.target.value) }   />
-                 </div>
-                 <div className="main">
-                      <span> Password: </span>
-                      <input type = "password" value = {pass}  onChange = { e => setpass(e.target.value) }  />
-                 </div>
-                 <button type = "submit" onClick={singin}>SignIn</button>
-                 <p> By continuing, you agree to Amazon's Conditions of Use and Privacy Notice. </p>
+              <div className="cover-content">
+  
+                      <div className="main_content" style = {{backgroundColor:'brown',padding:'3% 2%',margin:'1% 33%',width:'25%'}}> 
+                           <div className="cover" style = {{padding:'8% 5%'}}>
+                           <h1> Sign in  </h1>
+                          <div className="main">
+                                <span style = {{paddingRight:'7%'}}> Email: </span>
+                                <input type = "text"    value = {email}  onChange = { e => setemail(e.target.value) }   />
+                          </div>
+                          <div className="main">
+                                <span> Password: </span>
+                                <input type = "password" value = {pass}  onChange = { e => setpass(e.target.value) }  />
+                          </div>
+                           <button type = "submit" onClick={singin}>SignIn</button>
+                          <p> By continuing, you agree to Amazon's Conditions of Use and Privacy Notice. </p>
 
-                 <button type = "submit" onClick={register}> Create your Amazon Account </button>
-             </div>
-            
+                          <button type = "submit" onClick={register}> Create your Amazon Account </button>
+                           </div>
+                      
+                    </div>
+
+              </div>
+
         </div>
       </>
   )
